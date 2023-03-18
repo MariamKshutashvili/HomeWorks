bc4e97ccfb0fe0e32848bd13eed6376c626dbd4b
Mariam Kshutashvili <99123700+MariamKshutashvili@users.noreply.github.com>
2023-03-18 11:55:39 +0400

    N9 homework unfinished (#6)

---
+++ homework_N9/Homework__9/Homework__9/Employee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework__9
+{
+    internal class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string jobPosition;
+        public int age { get; set; }
+        public int averageHours;
+        public string dayHours;
+        public string JobPosition
+        {
+            get { return jobPosition; }
+            set { if (value == "manager" || value == "developer"
+                    || value == "tester" || value == "other")
+                {
+                    jobPosition= value;
+                } else
+                {
+                    Console.WriteLine("try again, something is wrong");
+                }            
+            }
+
+        }
+
+        public int AverageHours(int a, int s, int d, int f, int g, int h, int j)
+        {   
+            var averHours=(a+s+d+f+g+h+j)/7;
+            return averHours;
+        }
+
+        public string DayHours(int q, int w, int e, int r, int t, int y, int u)
+        {   
+            List <int> eachDayHours = new List <int>();
+            eachDayHours.Add(q);
+            eachDayHours.Add(w);
+            eachDayHours.Add(e);
+            eachDayHours.Add(r);
+            eachDayHours.Add(t);
+            eachDayHours.Add(y);
+            eachDayHours.Add(u);
+            return "";          
+            
+        }
+    }
+
+ }
---
+++ homework_N9/Homework__9/Homework__9/Student.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework__9
+{
+    internal class Student
+    {
+        public int ageofstudent { get; set; }
+        public string Name { get; set; }
+        public string lName { get; set; }
+        public int yearOfAcceptance;
+        
+        public int YearOfAcceptance(int yearOfAcceptance)
+        {
+            
+            return 4-(2023 - yearOfAcceptance); 
+            
+        }
+
+        public string RandomSubject(string a) 
+        {
+        
+        if (a == "1")
+            {
+              return "math";
+            }
+        if (a == "2")
+            {
+              return "English";
+            }
+        if (a == "3")
+            {
+              return "chemistry";
+            }
+            else
+            {
+                return "error. only 1, 2, 3";
+            }
+
+        }
+
+
+
+    }
+}
